f90b09957fffda4135a44604479983833b68f79c
Will <WilliamGao2004@gmail.com>
2023-12-15 18:11:51 -0500

    Add playRound function with test
    you pick either rock, paper, and scissor and it would go against the computer choice.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function getComputerChoice() {
   const randomChoice = Math.floor(Math.random() * 3);
 // Assign the number with either rock,paper, or scissor
 // Return the result
+  if (randomChoice === 0) {
     return "rock";
   } else if (randomChoice === 1) {
     return "paper";
@@ -11,3 +12,42 @@ function getComputerChoice() {
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+
+  //Makes playerSelection parameter case-insensitive
+  let playerChoice = playerSelection.toLowerCase();
+
+  // If you and the computer pick the same, it's a tie
+  if (playerChoice === computerSelection) {
+    return "It's a tie! Go Again";
+  }
+  // If you chose rock and they chose paper, you win
+  else if (playerChoice === "rock" && computerSelection === "paper") {
+    return "You Lose! Rock lose to Paper";
+  }
+  // If you chose rock and they chose scissor, you lose
+  else if (playerChoice === "rock" && computerSelection === "scissor") {
+    return "You Win! Rock beats Scissor";
+  }
+  // If you chose paper and they chose rock, you win
+  else if (playerChoice === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock"
+  }
+  // If you chose paper and they chose scissor, you lose
+  else if (playerChoice === "paper" && computerSelection === "scissor") {
+    return "You Lose! Paper lose to Scissor"
+  }
+  // If you chose scissor and they chose paper, you win
+  else if (playerChoice === "scissor" && computerSelection === "paper") {
+    return "You Win! Scissor beats Paper"
+  }
+  // If you chose scissor and they chose rock, you lose
+  else if (playerChoice === "scissor" && computerSelection === "rock") {
+    return "You lose! Scissor lose to Rock"
+  }
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
