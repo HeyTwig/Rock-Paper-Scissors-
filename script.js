e28e9fdb87f53c8e9ee3d3dad6b8c2e0a1843d74
Will <WilliamGao2004@gmail.com>
2023-12-16 10:37:53 -0500

    Add game function

--- script.js
+++ script.js
@@ -31,23 +31,68 @@ function playRound(playerSelection, computerSelection) {
   }
   // If you chose paper and they chose rock, you win
   else if (playerChoice === "paper" && computerSelection === "rock") {
-    return "You Win! Paper beats Rock"
+    return "You Win! Paper beats Rock";
   }
   // If you chose paper and they chose scissor, you lose
   else if (playerChoice === "paper" && computerSelection === "scissor") {
-    return "You Lose! Paper lose to Scissor"
+    return "You Lose! Paper lose to Scissor";
   }
   // If you chose scissor and they chose paper, you win
   else if (playerChoice === "scissor" && computerSelection === "paper") {
-    return "You Win! Scissor beats Paper"
+    return "You Win! Scissor beats Paper";
   }
   // If you chose scissor and they chose rock, you lose
   else if (playerChoice === "scissor" && computerSelection === "rock") {
-    return "You lose! Scissor lose to Rock"
+    return "You Lose! Scissor lose to Rock";
   }
 }
 
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+
+
+function game() {
+  // create a counter for both the computer and the player
+  let playerCounter = 0;
+  let computerCounter = 0;
+  // For loop the function playRound
+  for (let i = 0; i < 99; i++) {
+    // create a prompt that take in your answer
+    let playerSelection = prompt("Rock, Paper, or Scissor?");
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+
+    // After each round increase the count of the winner by one
+    if (result.includes("You Win!")) {
+      console.log(result)
+      playerCounter++;
+      console.log(`Player: ${playerCounter}`);
+    } else if (result.includes("You Lose!")) {
+      console.log(result)
+      computerCounter++;
+      console.log(`Computer: ${computerCounter}`);
+    }
+    // If tie, it prompt again with no changes to the counters
+      else {
+      console.log(result)
+    }
+    //It stops the loop after either the player or the computer gets 3 points
+    if (playerCounter === 3 || computerCounter === 3) {
+      break; 
+    }
+  }
+
+  // After one of the counter reaches 3, it display a winner
+  if (playerCounter === 3) {
+    console.log("You won against the computer!");
+    return;
+  } else if (computerCounter === 3) {
+    console.log("You lost against the computer :( Please Try Again");
+    return;
+  }
+}
+
+game();
